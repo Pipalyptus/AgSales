f4cb6e8f146444fc95223a42d0f64ed6fa018c14
Nico <nicolaspitchon@Nicos-MacBook-Air.local>
2019-06-01 14:00:16 -0700

    viewprofileCont start

--- client/src/containers/ViewProfileContainer.js
+++ client/src/containers/ViewProfileContainer.js
@@ -1,2 +1,29 @@
 import React, { Component } from 'react';
-import LoginPage from '../components/ViewProduct.js';
+import LoginPage from '../components/ViewProfile.js';
+
+export default class ViewProfilePageContainer extends Component {
+    constructor(props) {
+        super(props);
+    
+      }
+    
+      handleChange = event => {
+        this.setState({
+          [event.target.id]: event.target.value
+        });
+      };
+    
+      handleSubmit = event => {
+        event.preventDefault();
+      };
+
+  render() {
+    return (
+      <CreateProfilePage
+        handleSubmit={this.handleSubmit}
+        handleChange={this.handleChange}
+        changePage={this.props.changePage}
+      />
+    );
+  }
+}
